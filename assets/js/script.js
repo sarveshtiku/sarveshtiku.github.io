8fdad8192623eca14552be80c1121d2300157459
kohsheen1234 <64361696+sarveshtiku@users.noreply.github.com>
2024-08-19 04:42:29 -0600

    home page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,49 @@ window.addEventListener('DOMContentLoaded', function() {
         showContent('home-page'); // Show the home page by default if no hash is present
     }
 });
+
+
+function randomizeImagePosition() {
+    const images = document.querySelectorAll('.full-screen-gallery img');
+
+    images.forEach(img => {
+        const randomX = Math.floor(Math.random() * (window.innerWidth - img.clientWidth));
+        const randomY = Math.floor(Math.random() * (window.innerHeight - img.clientHeight));
+
+        img.style.left = `${randomX}px`;
+        img.style.top = `${randomY}px`;
+
+        // Randomize size (optional)
+        const randomSize = Math.random() * 10 + 10; // Between 10vw and 20vw
+        img.style.width = `${randomSize}vw`;
+        img.style.height = 'auto';
+    });
+}
+
+// Run the function once the page is loaded
+window.onload = randomizeImagePosition;
+
+// Run the function on window resize to adjust positions
+window.onresize = randomizeImagePosition;
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const welcomeText = document.getElementById('welcome-text');
+    const messages = ["Yo, what's good? 👋", "Glad you pulled up! 🙌", "You're chillin' at 🛋️", "Sarvesh Tiku's crib 🏠", "Feel free to vibe out 🎧"];
+
+
+    let messageIndex = 0;
+
+    function displayNextMessage() {
+        if (messageIndex < messages.length) {
+            welcomeText.innerHTML = messages[messageIndex];
+            messageIndex++;
+            setTimeout(displayNextMessage, 2000); // Change the message every 2 seconds
+        } else {
+            document.querySelector('.welcome-message').style.display = 'none';
+            document.querySelector('.constant-name').style.display = 'block';
+        }
+    }
+
+    displayNextMessage();
+});
